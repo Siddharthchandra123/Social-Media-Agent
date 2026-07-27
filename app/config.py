9e7281ff29537551d85429a307935eb6f62fcf49
Siddharth Chandra <chandrasiddharth975@gmail.com>
2026-07-27 22:27:52 +0530

    Add direct LinkedIn publishing

--- app/config.py
+++ app/config.py
@@ -29,8 +29,8 @@ class Settings(BaseSettings):
     )
     LINKEDIN_VERSION: str = "202603"
     LINKEDIN_ACCESS_TOKEN: str
-LINKEDIN_PLATFORM_USER_ID: str
-LINKEDIN_VERSION: str = "202506"
+    LINKEDIN_PLATFORM_USER_ID: str
+    LINKEDIN_VERSION: str = "202506"
 
     model_config = SettingsConfigDict(
         env_file=".env",
